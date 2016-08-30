86596e8ed6ce2b90b909b4ef2c9debe4e4da94c6
Yuriy Nemtsov <nemtsov@gmail.com>
2016-08-30 12:55:02 -0400

    send: handle environments that do not support Array#map

--- src/send.js
+++ src/send.js
@@ -1,8 +1,14 @@
+function encode(logs) {
+  var encodedLogs = [];
+  for (var i = 0; i < logs.length; i++) {
+    encodedLogs[i] = 'logs[]=' + encodeURIComponent(JSON.stringify(logs[i]));
+  }
+  return encodedLogs.join('&');
+}
+
 export default function send(logs) {
   var xhr = new XMLHttpRequest();
   xhr.open('POST', '/log');
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.send(logs.map(function(log) {
-    return 'logs[]=' + encodeURIComponent(JSON.stringify(log));
-  }).join('&'));
+  xhr.send(encode(logs));
 }
